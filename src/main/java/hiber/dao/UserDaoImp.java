fb5462f1746a9d7e9ac45fec3c00876c3423a6e6
SergejTkachenko <tks.emailbox@gmail.com>
2022-08-07 11:58:42 +0300

    Initial commit

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,49 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public User getUserWithCar(String model, int series) {
+      String hql = "from Car where model=:model and series=:series";
+      TypedQuery<Car> query = sessionFactory.getCurrentSession().createQuery(hql)
+              .setParameter("model", model)
+              .setParameter("series", series);
+
+      Car findCar = query.getSingleResult();
+      User findUser = findCar.getUser();
+
+      System.out.println("Id = " + findUser.getId());
+      System.out.println("First name = " + findUser.getFirstName());
+      System.out.println("Last name = " + findUser.getLastName());
+      System.out.println("Email = " + findUser.getEmail());
+
+      return findUser;
+   }
+
+}
